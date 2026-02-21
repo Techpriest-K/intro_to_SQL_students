62ad6ff6cda675cf8077b80ef47bd16df1bb7ac0
Techpriest-K <32102604+Techpriest-K@users.noreply.github.com>
2026-02-21 20:36:24 +0400

    Update solution.sql

--- task2/solution.sql
+++ task2/solution.sql
@@ -2,10 +2,11 @@ CREATE VIEW v_region_sales_summary AS
 SELECT
     Region,
     COUNT(*) AS Sales_Count,
-    SUM(
-        CAST(Unit_Price AS REAL) * CAST(Quantity_Sold AS INTEGER) * (1 - CAST(Discount AS REAL))
-    ) AS Total_Revenue
+    SUM(CAST(Unit_Price AS REAL) * CAST(Quantity_Sold AS REAL) * (1 - CAST(Discount AS REAL))) AS Total_Revenue
 FROM
     sales
 GROUP BY
-    Region;
+    Region
+ORDER BY
+    Total_Revenue DESC;
+
