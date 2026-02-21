d15781ebc2c6d08584ee154beb73a58c0d757885
Techpriest-K <32102604+Techpriest-K@users.noreply.github.com>
2026-02-21 20:44:47 +0400

    Update solution.sql

--- task3/solution.sql
+++ task3/solution.sql
@@ -3,19 +3,17 @@ ALTER TABLE server_logs ADD COLUMN Session_Dur REAL;
 UPDATE server_logs
 SET Session_Dur = (julianday(Session_End) - julianday(Session_Start)) * 24 * 60;
 
-
 CREATE VIEW v_users_activity AS
 SELECT
     u.User_ID,
     u.First_Name,
     u.Last_Name,
     COUNT(sl.Log_ID) AS Num_Sessions,
-    SUM(sl.Session_Dur) AS Total_Session_Time
+    COALESCE(SUM(sl.Session_Dur), 0) AS Total_Session_Time
 FROM
-    users
+    users u
 LEFT JOIN
-    server_logs sl
-    ON u.User_ID = sl.User_ID
+    server_logs sl ON u.User_ID = sl.User_ID
 GROUP BY
     u.User_ID, u.First_Name, u.Last_Name
 ORDER BY
