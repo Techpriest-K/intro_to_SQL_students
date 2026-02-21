518b5aa6b2cd9f984620ddd80a8a599abd382d65
Techpriest-K <32102604+Techpriest-K@users.noreply.github.com>
2026-02-21 20:26:19 +0400

    Update seed.sql

---
+++ task2/seed.sql
@@ -0,0 +1,11 @@
+CREATE TABLE sales (
+    Product_ID text,
+    Sale_Date text,
+    Sales_Rep text,
+    Region text,
+    Quantity_Sold text,
+    Product_Category text,
+    Unit_Cost text,
+    Unit_Price text,
+    Discount text
+);
